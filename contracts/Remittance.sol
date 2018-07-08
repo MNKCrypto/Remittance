83851eff021141ee64cc805769dd293aff11f161
Naveen <naveen.k.manickam@gmail.com>
2018-07-08 21:03:01 +0530

    Worked on Review Comments

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -3,20 +3,23 @@ pragma solidity ^0.4.21;
 contract Remittance {
     
     /* Data Structure to store individual payment requests */
-    struct Payment {
+    struct RemittanceStruct {
     uint etherValue;
+    address owner;
+    address exchange;
     bytes32 puzzle;
     uint  creationBlock;
-    uint paymentValidity;
+    uint paymentDuration;
    }
-   
-   uint maxValidity = 100;
+
+   bool isPaused = false;
+   uint maxDuration = 100;
    address contractOwner;
-   mapping(bytes32=>Payment) public payments;
+   mapping(bytes32=>RemittanceStruct) public payments;
    
    /* Events */ 
-   event LogRemittance(address indexed owner,address indexed exchange, address indexed receiver, bytes32 puzzle,uint value,uint validity);
-   event LogPayment(address indexed owner,address indexed exchange,address indexed receiver, uint value);
+   event LogRemittance(address indexed owner,address indexed exchange, bytes32 puzzle,uint value,uint duration);
+   event LogPayment(address indexed owner, address indexed exchange, bytes32 paymentHash, uint value);
    event LogKill(address indexed owner);
    
    /* Modifiers */
@@ -24,50 +27,71 @@ contract Remittance {
        require(msg.sender == contractOwner);
        _;
    }
+   modifier isContractActive {
+       require(!isPaused);
+       _;
+   }
   
-  /* Constructor - Storing the Contract Owner details. Only Contract Owner can kill this contract */
-  constructor(address _contractOwner) public {
-      contractOwner = _contractOwner;
+  /* Constructor - Storing the Contract Owner details. Only 
+  Contract Owner can pause/unpause this contract */
+  constructor() public {
+      contractOwner = msg.sender;
   } 
    
   /* createRemittance - Anyone can create new Remittance request. msg.sender will be the payer. 
-      @params : receiver - Payee who will receive the payment. 
-                exchange - Exchange through which payee is getting paid.
-                puzzle   - A puzzle which exchange and payee should resolve to open this payment
-                validity - defines for how many blocks (from current block) will the payment be available
+      @params : exchange    - Exchange through which payee is getting paid.
+                paymentHash - Hash of exchange address and receiver's OTP generated off-chain.
+                puzzle      - A puzzle which exchange and payee should resolve to open this payment
+                duration    - defines for how many blocks (from current block) will the payment be available
   */ 
-  function createRemittance(address receiver, address exchange, bytes32 puzzle, uint validity) public payable{
-        bytes32 paymentHash  = keccak256(receiver,exchange,msg.sender);
+  function createRemittance(address exchange, bytes32 paymentHash, bytes32 puzzle, uint duration) public payable
+            isContractActive {
+        require(paymentHash != 0);
         require((payments[paymentHash].etherValue) == 0);
+        require(duration <= maxDuration);
+        payments[paymentHash].exchange = exchange;
         payments[paymentHash].creationBlock = block.number;
-        require(validity <= maxValidity);
-        payments[paymentHash].paymentValidity= validity;
+        payments[paymentHash].paymentDuration= duration;
+        payments[paymentHash].owner = msg.sender;
         payments[paymentHash].etherValue = msg.value;
         payments[paymentHash].puzzle = puzzle;
-        emit LogRemittance(msg.sender,exchange,receiver,puzzle, msg.value,validity);
+        emit LogRemittance(msg.sender, exchange, puzzle, msg.value, duration);
     }
     
   /* payRemittance - Exchange will pass on their OTP along with the payee's OTP to reeive a payment. 
-     @params : paymentSource - Payer for this payment. 
-               beneficiary   -  Payee for this payment.
+     @params : paymentHash   - Hash of exchange address and receiver's OTP generated off-chain..
                exchangeOTP   - OTP received by the exchange.
                receiverOTP   - OTP received by the payee.
   */ 
-  function payRemittance (address paymentSource,address beneficiary, bytes32 exchangeOTP, bytes32 receiverOTP) public{
-        bytes32 paymentHash = keccak256(beneficiary,msg.sender,paymentSource);
+  function payRemittance (bytes32 paymentHash, bytes32 exchangeOTP, bytes32 receiverOTP) public isContractActive{
+        require(paymentHash!= 0);
         bytes32 passCode = keccak256(exchangeOTP,receiverOTP);
         require(payments[paymentHash].puzzle == passCode);
+        require(payments[paymentHash].paymentDuration >= (block.number - payments[paymentHash].creationBlock));
+        require(payments[paymentHash].etherValue > 0);
+        require(payments[paymentHash].exchange == msg.sender);
         uint etherAvailable = payments[paymentHash].etherValue;
-        require(payments[paymentHash].paymentValidity >= (block.number - payments[paymentHash].creationBlock));
-        require(etherAvailable > 0);
         payments[paymentHash].etherValue = 0;
         msg.sender.transfer(etherAvailable);
-        emit LogPayment(paymentSource,msg.sender,beneficiary,etherAvailable);
+        emit LogPayment(payments[paymentHash].owner,msg.sender,paymentHash,etherAvailable);
   }
     
-  /* killRemittanceContract - Contract Owner can invoke this function to destroy the contract. */
-  function killRemittanceContract() public onlyOwner{
-        selfdestruct(msg.sender);
-  }    
+  /* pauseRemittanceContract - Contract Owner can invoke this function to pause the contract. */
+  function pauseRemittanceContract() public onlyOwner{
+      isPaused = true;
+  }   
+  
+   /* resumeRemittanceContract - Contract Owner can invoke this function to resume the contract. */
+  function resumeRemittanceContract() public onlyOwner{
+      isPaused = false;
+  } 
+  
+  /* generatePuzzle - This function will be used internally by the contract (to validate
+                      the puzzle) as well as from the User side application (to generate
+                      the puzzle) */
+    function generatePuzzle(bytes32 exchangeOTP, bytes32 receiverOTP) pure returns(bytes32) {
+        return (keccak256(exchangeOTP,receiverOTP));
+  }
   
 }
+
